7bf6673fa0551f10f574cb60d3448da34bb39bdc
shubhamsingh8317 <62668720+shubhamsingh8317@users.noreply.github.com>
2023-05-18 10:00:02 +0530

    add task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import InputTodo from './InputTodo'
+import ToDoList from './ToDoList'
 
-function App() {
+
+const App = () => {
+  const[listTodo,setListTodo]=useState([])
+  const addList=(input)=>{
+   
+   setListTodo([...listTodo,input])
+  }
+
+  const deleteHandler=(key)=>{
+  const newList=[...listTodo]
+  newList.splice(key,1)
+  setListTodo([...newList])
+  }
+  //console.log(listTodo)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+   <InputTodo addData={addList} enterKey={onEnterKey}/>
+   <h1>Todos</h1>
+   <hr/>
+   <div className='container'> 
+   {
+    listTodo.map((listitem,i)=>{
+     
+    return  <ToDoList key={i} index={i} eachItem={listitem} del={deleteHandler}/>
+      
+    })
+   }
     </div>
-  );
+    </div>
+  )
 }
 
-export default App;
+export default App
